cc63546d15e49a6f188a37a279ce9ba87b4958e8
Jose Garcia <jagarcia7655@gmail.com>
2020-09-15 18:40:54 -0500

    Aprendiendo módulos externos buffers y streams

---
+++ memoria/buffer.js
@@ -0,0 +1,15 @@
+let buffer = Buffer.alloc(5)
+let bufferLlenos = Buffer.from([1,2,3,4])
+let bufferHola = Buffer.from('Holi')
+
+console.log(buffer);
+console.log(bufferLlenos);
+console.log(bufferHola.toString());
+
+let abc = Buffer.alloc(26);
+
+for (let i = 0; i < abc.length; i++) {
+  abc[i] = i + 97;
+}
+
+console.log(abc.toString());
---
+++ memoria/streams.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const stream = require('stream');
+const util = require('util')
+
+let readableStream = fs.createReadStream(__dirname + "/input.txt");
+readableStream.setEncoding("UTF8");
+
+const Transform = stream.Transform;
+
+function Mayus () {
+  Transform.call(this)
+};
+
+util.inherits(Mayus, Transform)
+
+Mayus.prototype._transform = function (chunk, codif, cb) {
+  chunkMayus = chunk.toString().toUpperCase();
+  this.push(chunkMayus);
+  cb();
+};
+
+let mayus = new Mayus();
+
+readableStream
+  .pipe(mayus)
+  .pipe(process.stdout)
+
+
+
+// let data = '';
+
+
+
+// readableStream.on('data', (chunk) => {
+//   data += chunk;
+// });
+
+// readableStream.on('end', () => {
+//   console.log(data);
+// });
+
+// process.stdout.write('Hola')
+// process.stdout.write('Que')
+// process.stdout.write('Tal')
